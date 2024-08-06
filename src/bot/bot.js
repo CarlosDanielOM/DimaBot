9a4ee1d31f8149475b87d3feae71158ca229020f
CDOM201 <carlos@carlosdaniel.info>
2024-08-05 22:16:46 -0700

    Bot index

--- src/bot/bot.js
+++ src/bot/bot.js
@@ -4,10 +4,7 @@ const CLIENT = require('../../util/client');
 const STREAMERS = require('../../class/streamer');
 const {connectChannels, refreshAllTokens} = require('../../util/dev')
 const token = require('../../util/token');
-const addModerator = require('../../command/addmoderator');
-const ruletarusa = require('../../command/ruletarusa');
-const shoutout = require('../../command/shoutout');
-const speachChat = require('../../command/speach');
+const messageHandler = require('../../handler/message');
 
 let client = null;
 
@@ -18,7 +15,56 @@ async function init() {
 
     await refreshAllTokens(token.refreshAllTokens)
 
-    console.log(await speachChat('533538623', {username: 'sleeples_panda', id: Math.random() * 999999999}, 'Hola'));
+    client.on('resub', (channel, username, months, message, userstate, methods) => {
+        let tier = userstate['msg-param-sub-plan'];
+        switch (tier) {
+            case 'Prime':
+                tier = 'Prime';
+                break;
+            case '1000':
+                tier = '1';
+                break;
+            case '2000':
+                tier = '2';
+                break;
+            case '3000':
+                tier = '3';
+                break;
+        }
+
+        if (months == 0) { months++ }
+
+        client.say(channel, `Gracias por la resub ${username}! de ${months} meses y de nivel ${tier}!`);
+    });
+
+    client.on('subscription', (channel, username, method, message, userstate) => {
+        let tier = userstate['msg-param-sub-plan'];
+        switch (tier) {
+            case 'Prime':
+                tier = 'Prime';
+                break;
+            case '1000':
+                tier = '1';
+                break;
+            case '2000':
+                tier = '2';
+                break;
+            case '3000':
+                tier = '3';
+                break;
+        }
+
+        client.say(channel, `Gracias por la sub ${username} de nivel ${tier}!`);
+    })
+
+    client.on('message', async (channel, tags, message, self) => {
+        if(self) return;
+        await messageHandler(client, channel.replace('#', ''), tags, message);
+    })
+
+    client.on('cheer', (channel, tags, message) => {
+        client.say(channel, `Gracias por los ${tags.bits} bits ${tags['display-name']}!`)
+    });
 }
 
 module.exports = init;

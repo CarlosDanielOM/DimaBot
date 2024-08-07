30e42593f7240d984cd585e22b709e71cb37f558
CDOM201 <carlos@carlosdaniel.info>
2024-08-07 13:54:17 -0700

    Fixes

--- handler/raid.js
+++ handler/raid.js
@@ -5,7 +5,7 @@ const modID = '698614112';
 
 async function raid(client, eventData, eventsubData, clipEnabled = false) {
     if (eventsubData.minViewers > eventData.viewers) return;
-    const { broadcaster_user_id, broadcaster_user_login, from_broadcaster_user_id, from_broadcaster_user_name } = eventData;
+    const { to_broadcaster_user_id, to_broadcaster_user_login, from_broadcaster_user_id, from_broadcaster_user_name } = eventData;
 
     let so = await shoutout(to_broadcaster_user_id, from_broadcaster_user_name, 'purple');
     

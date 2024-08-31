425a8db11860b637731bfbae9fb21d0df0773364
CDOM201 <carlos@carlosdaniel.info>
2024-08-30 23:13:40 -0700

    Update command.js

--- handler/command.js
+++ handler/command.js
@@ -160,7 +160,8 @@ async function specialCommands(channelID, tags, argument, cmdFunc, count = 0) {
                 }
                 break;
             case 'raid':
-                let raid = special[2] || argument;
+                let raid = special[3] || argument;
+                console.log({raid, special});
                 let raidUserData = await getUserByLogin(raid);
                 if(raidUserData.error) {
                     cmdFunc = cmdFunc.replace(special[0], raidUserData.message);

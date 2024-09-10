a8b6994479373563ebdff0e32821658db0f8946f
CDOM201 <carlos@carlosdaniel.info>
2024-09-10 14:47:50 -0700

    Update logger.js

--- util/logger.js
+++ util/logger.js
@@ -4,10 +4,11 @@ async function logger(data, cache = false, channelID = null, type = null) {
     let cacheClient = getClient();
 
     if(cache) {
-        await cacheClient.set(`logger:${channelID}:${type}`, JSON.stringify({data, timestamp: Date.now()}));
+        await cacheClient.set(`logger:${channelID}:${type}`, JSON.stringify({data, timestamp: Date.now().toLocaleString('en-US')}));
+        await cacheClient.expire(`logger:${channelID}:${type}`, 60 * 60 * 24 * 7);
     }
 
-    console.log({data, timestamp: Date.now()})
+    console.log({data, timestamp: Date.now().toLocaleString('en-US')})
     
 }
 

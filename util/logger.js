7d4c221b6b7813dd2d1f9c564fbd52c27d5d464c
CDOM201 <carlos@carlosdaniel.info>
2024-09-10 14:51:43 -0700

    Update logger.js

--- util/logger.js
+++ util/logger.js
@@ -4,12 +4,12 @@ async function logger(data, cache = false, channelID = null, type = null) {
     let cacheClient = getClient();
 
     if(cache) {
-        let uniqueID = `${channelID}-${type}-${Date.now().toLocaleString('en-US')}`;
-        await cacheClient.set(`logger:${channelID}:${type}:${uniqueID}`, JSON.stringify({data, timestamp: Date.now().toLocaleString('en-US')}));
+        let uniqueID = `${channelID}-${type}-${Date.now().toLocaleString('en-US', {timeZone: 'UTC'})}`;
+        await cacheClient.set(`logger:${channelID}:${type}:${uniqueID}`, JSON.stringify({data, timestamp: Date.now().toLocaleString('en-US', {timeZone: 'UTC'})}));
         await cacheClient.expire(`logger:${channelID}:${type}:${uniqueID}`, 60 * 60 * 24 * 7);
     }
 
-    console.log({data, timestamp: Date.now().toLocaleString('en-US')})
+    console.log({data, timestamp: Date.now().toLocaleString('en-US', {timeZone: 'UTC'})})
     
 }
 

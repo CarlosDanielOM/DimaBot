c1172a831c3bcb28e4bdf344c2989d5146ea4f9f
CDOM201 <carlos@carlosdaniel.info>
2024-09-23 22:39:02 -0700

    Update video.routes.js

--- src/server/routes/video.routes.js
+++ src/server/routes/video.routes.js
@@ -4,13 +4,13 @@ const fs = require('fs');
 
 router.get('/clip/:channelID', (req, res) => {
     const channelID = req.params.channelID;
-    res.status(200).sendFile(`${__dirname}/routes/public/downloads/${channelID}-clip.mp4`);
+    res.status(200).sendFile(`${__dirname}/public/downloads/${channelID}-clip.mp4`);
 });
 
 router.get('/trigger/:channelID/:triggerName', (req, res) => {
     const channelID = req.params.channelID;
     const triggerName = req.params.triggerName;
-    if(!fs.existsSync(`${__dirname}/routes/public/uploads/triggers/${channelID}/${triggerName}.mp4`)) {
+    if(!fs.existsSync(`${__dirname}/public/uploads/triggers/${channelID}/${triggerName}.mp4`)) {
         return res.status(404).send({
             error: 'File not found',
             message: `The file ${triggerName} does not exist in the ${channelID} channel`

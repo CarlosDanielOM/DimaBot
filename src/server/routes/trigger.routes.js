02d1462174e868d835f4027b3f0ec14f89cccf41
CDOM201 <carlos@carlosdaniel.info>
2024-11-01 23:16:44 -0700

    Update trigger.routes.js

--- src/server/routes/trigger.routes.js
+++ src/server/routes/trigger.routes.js
@@ -286,10 +286,8 @@ router.delete('/:channelID/:triggerID', async (req, res) => {
         });
     }
 
-    let streamer = await STREAMERS.getStreamerById(channelID);
-
     let streamerHeaders = await getStreamerHeaderById(channelID);
-    let streamerToken = await streamer.getStreamerTokenById(channelID);
+    let streamerToken = await STREAMERS.getStreamerTokenById(channelID);
 
     let response = await fetch(`${getUrl()}/rewards/${channelID}/${trigger.rewardID}`, {
         method: 'DELETE',

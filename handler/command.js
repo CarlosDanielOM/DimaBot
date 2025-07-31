daa0e53d7211e9f2960e37b8a8a2c74cfded6d74
CDOM201 <carlos@carlosdaniel.info>
2025-07-30 22:46:14 -0700

    tests

--- handler/command.js
+++ handler/command.js
@@ -69,6 +69,7 @@ async function specialCommands(channelID, tags, argument, cmdFunc, count = 0) {
                     break;
                 case 'vip':
                     let user = special[2] || argument;
+                    console.log({user, argument, special});
                     let vipAction = await addVIPCommand(channelID, user, tags);
                     if(vipAction.error) {
                         cmdFunc = cmdFunc.replace(special[0], vipAction.message);
